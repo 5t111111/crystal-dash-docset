19e8366bd916fee38b4e3594b0e0de0de33e49b5
Hirofumi Wakasugi <baenej@gmail.com>
2015-12-03 13:09:53 +0900

    Github pushes

--- travis_build.sh
+++ travis_build.sh
@@ -7,4 +7,15 @@ export PATH=${GOPATH}/bin:$PATH
 crystal-dash-docset-generator
 pushd crystal
 ls -l
+popd
 
+git config --global user.email "baenej@gmail.com"
+git config --global user.name "Hirofumi Wakasugi"
+
+git remote add upstream "https://$GH_TOKEN@github.com/5t111111/crystal-dash-docset"
+git fetch upstream
+git reset upstream/master
+
+git add -A
+git commit -m "rebuild docset [ci skip]" || true
+git push -q upstream HEAD:master
